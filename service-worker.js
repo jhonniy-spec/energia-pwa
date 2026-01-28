9406272e7b5be6cc320858e46eb69aecf41bda22
jhonniy-spec <plucinskijhonata@gmail.com>
2026-01-27 23:22:58 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "energia-pwa-v1";
+const FILES = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
